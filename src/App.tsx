980087bac4adb96af7575378ca33d74d9d8b85e3
yuyalwinoo <yuyal1593@gmail.com>
2025-03-12 10:50:47 +0630

    [counter with zustand]

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useCounterStore } from "./store"
+
 const App = () => {
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
   return (
-    <div>App</div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1>Counter with Zustand</h1>
+      <p>{count}</p>
+      <div className="flex space-x-3">
+        <button onClick={increment} className="px-3 py-2 bg-blue-400 rounded-xl cursor-pointer">+</button>
+        <button onClick={decrement} className="px-3 py-2 bg-blue-400 rounded-xl cursor-pointer">-</button>
+      </div>
+    </div>
   )
 }
 
